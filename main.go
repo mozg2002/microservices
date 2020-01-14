6ccebf5d6bfe8cf88eb13eba92589a47c752da39
mozg2002 <mozg2002@sina.com>
2020-01-14 23:42:41 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+
+	pb "github.com/mozg2002/microservices/consignment/proto/consigment"
+	"google.golang.org/grpc"
+	"google.golang.org/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Ulock()
+	return consignment, nil
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatelf()
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterSippingserviceServer(s, &service{repo})
+
+	reflection.Register(s)
+
+	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
